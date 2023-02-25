e4b3beed60bbfc814bc4886314791c12734b6942
tsubasa-ptcs <tubasa.adachi@persol.co.jp>
2023-02-26 07:07:34 +0900

    [update]試し11

--- src/common/navigation.tsx
+++ src/common/navigation.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Nav, Navbar } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 export default function Navigation() {
   return (
@@ -11,15 +12,15 @@ export default function Navigation() {
         variant="dark"
         className="col-12"
       >
-        <Navbar.Brand href="/">
+        <Navbar.Brand as={NavLink} to="/">
           {/* <img src={LOGO} width="30" height="30" alt="React Bootstrap logo" /> */}
           React Study
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/content1">Reactって？</Nav.Link>
-            <Nav.Link href="/content2">Reactを書いてみる</Nav.Link>
+            <Nav.Link as={NavLink} to="/content1">Reactって？</Nav.Link>
+            <Nav.Link as={NavLink} to="/content2">Reactを書いてみる</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
